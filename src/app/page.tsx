641010270a3c1227e37fb64514a78fd566407994
ABDIFATAH DAHIR MUSE <abdifataah1619@gmail.com>
2023-08-30 11:47:47 +0300

    welcome section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  bg-green-300  ">
+    <main className="flex min-h-screen flex-col    ">
       <Welcome />
     </main>
   );
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,5 +1,29 @@
 import React from "react";
-
+import { PiFilmSlateFill } from "react-icons/pi";
+import { BsBook, BsPen } from "react-icons/bs";
+import { AiOutlineBulb } from "react-icons/ai";
 export default function Welcome() {
-  return <div>Welcome</div>;
+  return (
+    <div className="  flex justify-center    ">
+      <div className=" w-[90%]  h-32 md:h-48 lg:w-full flex  flex-col gap-3  md:gap-2 md:py-4 items-center justify-center bg-gray-100 text-center rounded-lg">
+        <h1 className="text-slate-500 text-base font-medium md:text-2xl">
+          Welcome to our Blog
+        </h1>
+        <div className="md:w-[65%] md:h-[80%]  md:p-2">
+          <h2 className="text-base font-medium md:text-lg lg:text-2xl xl:text-4xl ">
+            Crafting narrative that ignit{" "}
+            <span className="text-red-500"> inspiration</span>,{" "}
+            <span className="text-red-500">knowledge</span> , and
+            <span className="text-red-500"> entertainment </span>
+          </h2>
+        </div>
+        <div className="flex gap-1 md:gap-3">
+          <PiFilmSlateFill className="w-10 h-6 md:h-8" />
+          <BsBook className="w-10 h-6 md:h-8" />
+          <AiOutlineBulb className="w-10 h-6 md:h-8" />
+          <BsPen className="w-10 h-6 md:h-8" />
+        </div>
+      </div>
+    </div>
+  );
 }
